a26f9995393ab3259c9b4f7c3502dd2d6b6b9d8a
DenisKincses <78603495+DenisKincses@users.noreply.github.com>
2023-01-30 20:59:25 +0200

    soundcloud.js
    Scratch Extension with Soundcloud Functionality

---
+++ soundcloud.js
@@ -0,0 +1,32 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            [' ', 'play SoundCloud track %s', 'playSoundCloud', 'https://soundcloud.com/track']
+        ],
+        url: 'https://chat.openai.com/chat'
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Sample extension', descriptor, ext);
+
+    // Block functions
+    ext.playSoundCloud = function(url) {
+        var soundcloudSdk = document.createElement('script');
+        soundcloudSdk.src = "https://connect.soundcloud.com/sdk/sdk-3.3.2.js";
+        document.head.appendChild(soundcloudSdk);
+        
+        SC.oEmbed(url, {auto_play: true}).then(function(oEmbed) {
+            var soundcloudPlayer = $('<div>').html(oEmbed.html).appendTo('body');
+        });
+    };
+})({});
