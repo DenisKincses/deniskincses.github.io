743fdf7c3dd3eeb96d2396da87e43c33568cc6a9
DenisKincses <78603495+DenisKincses@users.noreply.github.com>
2023-02-13 11:34:48 +0200

    Update windowmove.js
    Signed-off-by: DenisKincses <78603495+DenisKincses@users.noreply.github.com>

--- windowmove.js
+++ windowmove.js
@@ -9,14 +9,14 @@
         var windowHeight = window.innerHeight;
         var screenWidth = screen.width;
         var screenHeight = screen.height;
-        window.moveTo(x + (screenWidth - windowWidth) / 2, y + (screenHeight - windowHeight) / 2);
+        window.moveTo(x + (screenWidth - windowWidth) / 2, -y + (screenHeight - windowHeight) / 2);
     };
     
     var descriptor = {
         blocks: [
             [' ', 'set window x: %n y: %n', 'set_window_position', 0, 0]
         ],
-        url: 'http://scratch.mit.edu'
+        url: 'https://deniskincses.github.io/'
     };
     
     ScratchExtensions.register('Window position', descriptor, ext);
