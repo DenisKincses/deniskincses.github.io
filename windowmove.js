d888bf2302fab4da83810555f50a34101cb74cd3
DenisKincses <78603495+DenisKincses@users.noreply.github.com>
2023-02-13 11:43:43 +0200

    Update windowmove.js
    bug where the project lags down, and made it so it would wait 0 seconds after the move to line
    
    Signed-off-by: DenisKincses <78603495+DenisKincses@users.noreply.github.com>

--- windowmove.js
+++ windowmove.js
@@ -4,19 +4,22 @@
         return {status: 2, msg: 'Ready'};
     };
     
-    ext.set_window_position = function(x, y) {
+    ext.set_window_position = function(x, y, callback) {
         var windowWidth = window.innerWidth;
         var windowHeight = window.innerHeight;
         var screenWidth = screen.width;
         var screenHeight = screen.height;
         window.moveTo(x + (screenWidth - windowWidth) / 2, -y + (screenHeight - windowHeight) / 2);
+        setTimeout(function() {
+            callback();
+        }, 0);
     };
     
     var descriptor = {
         blocks: [
-            [' ', 'set window x: %n y: %n', 'set_window_position', 0, 0]
+            ['w', 'set window x: %n y: %n', 'set_window_position', 0, 0, 'wait']
         ],
-        url: 'https://deniskincses.github.io/'
+        url: 'http://scratch.mit.edu'
     };
     
     ScratchExtensions.register('Window position', descriptor, ext);
