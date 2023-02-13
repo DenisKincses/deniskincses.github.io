c5642492d80f1c870669c15d009fa78d6948313f
DenisKincses <78603495+DenisKincses@users.noreply.github.com>
2023-02-13 17:31:04 +0200

    Create alert.js
    Signed-off-by: DenisKincses <78603495+DenisKincses@users.noreply.github.com>

---
+++ alert.js
@@ -0,0 +1,35 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.alert = function(text) {
+        window.alert(text);
+    };
+
+    ext.confirm = function(text) {
+        return window.confirm(text);
+    };
+
+    ext.prompt = function(text, defaultText) {
+        return window.prompt(text, defaultText);
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            [' ', 'alert %s', 'alert', 'Hello! How are you?'],
+            ['b', 'confirm %s', 'confirm', 'Do you want to continue?'],
+            ['r', 'prompt %s with default %s', 'prompt', 'Enter your name', 'Scratch User']
+        ],
+        url: 'https://github.com/scratch-extensions/alert-extension'
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Alert Extension', descriptor, ext);
+})({});
