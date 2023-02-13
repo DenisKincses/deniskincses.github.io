61f3f9f4ca60209e94b6115bf00a02d904b9405f
DenisKincses <78603495+DenisKincses@users.noreply.github.com>
2023-02-13 18:50:33 +0200

    Update embedpage.js
    Signed-off-by: DenisKincses <78603495+DenisKincses@users.noreply.github.com>

--- extensions/embedpage.js
+++ extensions/embedpage.js
@@ -1,58 +1,70 @@
 (function(ext) {
-var stageWidth, stageHeight;
-var iframe;
+    var stageWidth, stageHeight;
+    var iframe;
     
-ext.embed_webpage = function(url) {
-    if (!iframe) {
-        iframe = document.createElement('iframe');
-        document.body.appendChild(iframe);
-        iframe.style.position = 'absolute';
-    }
+    ext.embed_webpage = function(url) {
+        if (!iframe) {
+            iframe = document.createElement('iframe');
+            document.body.appendChild(iframe);
+            iframe.style.position = 'absolute';
+        }
     
-    stageWidth = window.innerWidth;
-    stageHeight = window.innerHeight;
-    var offsetX = stageWidth / 2;
-    var offsetY = stageHeight / 2;
+        stageWidth = window.innerWidth;
+        stageHeight = window.innerHeight;
+        var offsetX = stageWidth / 2;
+        var offsetY = stageHeight / 2;
     
-    iframe.style.width = stageWidth + 'px';
-    iframe.style.height = stageHeight + 'px';
-    iframe.style.left = offsetX + 'px';
-    iframe.style.top = offsetY + 'px';
-    iframe.src = url;
-};
-
-ext.set_webpage_position = function(x, y) {
-    if (!iframe) {
-        return;
-    }
-    iframe.style.left = x + offsetX + 'px';
-    iframe.style.top = (-1 * y) + offsetY + 'px';
-};
-
-ext.set_webpage_dimensions = function(width, height) {
-    if (!iframe) {
-        return;
-    }
-    iframe.style.width = width + 'px';
-    iframe.style.height = height + 'px';
-};
-
-ext.remove_webpage = function() {
-    if (iframe) {
-        document.body.removeChild(iframe);
-        iframe = null;
-    }
-};
-
-var descriptor = {
-    blocks: [
-        [' ', 'embed webpage %s', 'embed_webpage', 'https://www.example.com'],
-        [' ', 'set webpage position x: %n y: %n', 'set_webpage_position', 0, 0],
-        [' ', 'set webpage size width: %n height: %n', 'set_webpage_size', 480, 360],
-        [' ', 'remove webpage', 'remove_webpage']
-    ],
-    url: 'https://deniskincses.github.io/'
-};
-
-ScratchExtensions.register('Embed Page', descriptor, ext);
+        iframe.style.width = stageWidth + 'px';
+        iframe.style.height = stageHeight + 'px';
+        iframe.style.left = offsetX + 'px';
+        iframe.style.top = offsetY + 'px';
+        iframe.src = url;
+    };
+    
+    ext.set_webpage_position = function(x, y) {
+        if (!iframe) {
+            return;
+        }
+        iframe.style.left = x + offsetX + 'px';
+        iframe.style.top = (-1 * y) + offsetY + 'px';
+    };
+    
+    ext.set_webpage_dimensions = function(width, height) {
+        if (!iframe) {
+            return;
+        }
+        iframe.style.width = width + 'px';
+        iframe.style.height = height + 'px';
+    };
+    
+    ext.remove_webpage = function() {
+        if (iframe) {
+            document.body.removeChild(iframe);
+            iframe = null;
+        }
+    };
+    
+    window.addEventListener("resize", function() {
+        stageWidth = window.innerWidth;
+        stageHeight = window.innerHeight;
+        var offsetX = stageWidth / 2;
+        var offsetY = stageHeight / 2;
+    
+        iframe.style.width = stageWidth + 'px';
+        iframe.style.height = stageHeight + 'px';
+        iframe.style.left = offsetX + 'px';
+        iframe.style.top = offsetY + 'px';
+    });
+    
+    var descriptor = {
+        blocks: [
+            [' ', 'embed webpage %s', 'embed_webpage', 'https://www.example.com'],
+            [' ', 'set webpage position x: %n y: %n', 'set_webpage_position', 0, 0],
+            [' ', 'set webpage size width: %n height: %n', 'set_webpage_size', 480, 360],
+            [' ', 'remove webpage', 'remove_webpage']
+        ],
+        url: 'https://deniskincses.github.io/'
+    };
+    
+    ScratchExtensions.register('Embed Page', descriptor, ext);
 })({});
