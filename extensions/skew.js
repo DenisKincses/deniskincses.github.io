4bd1760354aa663be1b224e7f546fb577548e19c
DenisKincses <78603495+DenisKincses@users.noreply.github.com>
2023-03-02 19:57:57 +0200

    Update skew.js
    Signed-off-by: DenisKincses <78603495+DenisKincses@users.noreply.github.com>

--- extensions/skew.js
+++ extensions/skew.js
@@ -1,21 +1,20 @@
-(function(ext) {
+(function (ext) {
+  // Define block and its behavior for skewing the sprite
+  ext.skew = function (angleX, angleY) {
+    // Skew the sprite by the specified angles
+    var skewX = Math.tan(angleX * Math.PI / 180);
+    var skewY = Math.tan(angleY * Math.PI / 180);
+    var style = "skew(" + skewX + "rad," + skewY + "rad)";
+    ScratchExtensions.runtime._sprite.style.transform = style;
+  };
 
-    // define skew block
-    ext.skew = function(spriteIndex, skewX, skewY) {
-        var sprite = Scratch.sprite[spriteIndex];
-        sprite.scratchSkewX = skewX;
-        sprite.scratchSkewY = skewY;
-        sprite.draw();
-    };
+  // Block and descriptor for skewing the sprite
+  var descriptor = {
+    blocks: [
+      [' ', 'Skew the sprite by X: %n Y: %n', 'skew', 0, 0],
+    ],
+  };
 
-    // block and descriptor definitions
-    var descriptor = {
-        blocks: [
-            [' ', 'skew sprite %n by x:%n y:%n', 'skew', 1, 0, 0]
-        ]
-    };
-
-    // register the extension
-    ScratchExtensions.register('Skew extension', descriptor, ext);
-
+  // Register the extension
+  ScratchExtensions.register('skew', descriptor, ext);
 })({});
